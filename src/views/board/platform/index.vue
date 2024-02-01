ed5f7e091a75a9f625923340173a7df96b077d19
liu yang <631515751@qq.com>
2024-02-01 16:02:11 +0800

    fix: 修改编辑回显

--- src/views/board/platform/index.vue
+++ src/views/board/platform/index.vue
@@ -1,28 +1,11 @@
 <template>
-  <div>
-    <BasicTable @register="registerTable" />
-  </div>
+  <BasicTable @register="registerTable" />
 </template>
 <script lang="ts" setup>
   import { BasicTable, useTable } from '@/components/Table';
-  import { reactive } from 'vue';
   import { promote } from '@/api/board';
   import { getBasicColumns, getWithDrawFormConfig } from './tableData';
 
-  const state = reactive<{
-    selectedRowKeys: any;
-    reject: string;
-    pagination: Record<string, any>;
-  }>({
-    selectedRowKeys: [], // Check here to configure the default column
-    pagination: { pageSize: 20 },
-    reject: '',
-  });
-
-  const onSelectChange = (ids) => {
-    console.log(ids);
-    state.selectedRowKeys = ids;
-  };
   const [registerTable] = useTable({
     title: '订单数据',
     api: promote,
@@ -33,14 +16,6 @@
     tableSetting: { fullScreen: true },
     showIndexColumn: false,
     rowKey: 'id',
-    fetchSetting: {
-      listField: 'list',
-      totalField: 'totalRecords',
-    },
-    rowSelection: {
-      type: 'checkbox',
-      onChange: onSelectChange,
-    },
     showSelectionBar: true, // 显示多选状态栏
     pagination: { pageSize: 20 },
   });
